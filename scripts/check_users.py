56f86e939a3ca5a8c83c0a544344df0194384915
Brad Winfield <bwinfield@vmware.com>
2023-05-31 16:22:25 -0500

    Don't create tkg-admin.

--- scripts/check_users.py
+++ scripts/check_users.py
@@ -12,6 +12,9 @@ def dprint(msg):
 
 def check_vcenter_user(server, token, username, password):
     found_user = False
+    if "@na.corp" in username:
+        pmsg.warning("Users in @na.corp... not manageable with the vCenter API.")
+        return False
     if "@vsphere.local" in username:
         pmsg.warning("Users in @vsphere.local not manageable with the vCenter API.")
         return False
